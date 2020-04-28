ad0b166b6e249d1bcc562341ab6ae45df76bf468
전현성 <skyoun97@gmail.com>
2020-04-28 23:16:44 +0900

    Backend - Adding followers/following count in User

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -2,16 +2,22 @@ import {prisma} from "../../../generated/prisma-client";
 
 export default {
   User: {
-    fullName: parent => {
+    fullName: (parent) => {
       return `${parent.firstName} ${parent.lastName}`;
     },
     isFollowing: (parent, _, {request}) => {
       const {user} = request;
       const {id: parentId} = parent;
       return prisma.$exists.user({
-        AND: [{id: user.id}, {following_some: {id: parentId}}]
+        AND: [{id: user.id}, {following_some: {id: parentId}}],
       });
     },
-    isSelf: (parent, _, {request}) => parent.id === request.user.id
-  }
+    isSelf: (parent, _, {request}) => parent.id === request.user.id,
+  },
+  followersCount: (parent) => {
+    return parent.followers.length;
+  },
+  followingCount: (parent) => {
+    return parent.following.length;
+  },
 };
--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -1,5 +1,4 @@
 import {prisma} from "../../../../generated/prisma-client";
-import {USER_FRAGMENT} from "../../../fragments";
 
 export default {
   Query: {
