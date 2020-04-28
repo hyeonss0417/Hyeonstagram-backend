5190b3b9b643672601cfdc0039e38e766f277807
전현성 <skyoun97@gmail.com>
2020-04-28 23:28:28 +0900

    Backend - Bug Fixing seeUser by username, not id

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -3,10 +3,10 @@ import {prisma} from "../../../../generated/prisma-client";
 export default {
   Query: {
     seeUser: async (_, args) => {
-      const {id} = args;
-      const user = await prisma.user({id});
-      user["posts"] = await prisma.user({id}).posts();
+      const {username} = args;
+      const user = await prisma.user({username});
+      user["posts"] = await prisma.user({username}).posts();
       return user;
-    }
-  }
+    },
+  },
 };
